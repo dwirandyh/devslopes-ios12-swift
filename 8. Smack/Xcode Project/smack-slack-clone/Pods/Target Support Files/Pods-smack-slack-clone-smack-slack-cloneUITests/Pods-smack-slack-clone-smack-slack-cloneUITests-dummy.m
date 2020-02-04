b59ae119ef8061986de17d7573a8586caf496d17
Dwi Randy Herdinanto <dwirandyherdinanto@gmail.com>
2020-02-04 10:14:46 +0700

    SMACK: Initialize Project

---
+++ 8. Smack/Xcode Project/smack-slack-clone/Pods/Target Support Files/Pods-smack-slack-clone-smack-slack-cloneUITests/Pods-smack-slack-clone-smack-slack-cloneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smack_slack_clone_smack_slack_cloneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_smack_slack_clone_smack_slack_cloneUITests
+@end
---
+++ 8. Smack/Xcode Project/smack-slack-clone/smack-slack-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack-slack-clone
+//
+//  Created by Dwi Randy Herdinanto on 03/02/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
