4cb526688c59ad0b2f32e1bad955c9eceb1f6fb4
Dwi Randy Herdinanto <dwirandyherdinanto@gmail.com>
2020-02-11 09:52:18 +0700

    Add Login View Controller

---
+++ 8. Smack/Xcode Project/smack-slack-clone/smack-slack-clone/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  smack-slack-clone
+//
+//  Created by Dwi Randy Herdinanto on 11/02/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ 8. Smack/Xcode Project/smack-slack-clone/smack-slack-clone/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack-slack-clone
+//
+//  Created by Dwi Randy Herdinanto on 11/02/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
