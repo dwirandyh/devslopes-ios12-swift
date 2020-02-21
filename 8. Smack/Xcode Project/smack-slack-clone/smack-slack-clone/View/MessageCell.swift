8015e1e0fce06038dcc7d88328aecf21ca47cdda
Dwi Randy Herdinanto <dwirandyherdinanto@gmail.com>
2020-02-21 10:47:15 +0700

    Fix(Smack): Display formatted date chat

--- 8. Smack/Xcode Project/smack-slack-clone/smack-slack-clone/View/MessageCell.swift
+++ 8. Smack/Xcode Project/smack-slack-clone/smack-slack-clone/View/MessageCell.swift
@@ -33,5 +33,19 @@ class MessageCell: UITableViewCell {
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         
+        // 2017-07-13T21:49:25.590Z
+        guard var isoDate = message.timeStamp else { return }
+        isoDate = String(isoDate.dropLast(5))
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timestampLabel.text = finalDate
+        }
     }
 }
